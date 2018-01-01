c389146c3bba7861767e4f639eedee1e3dafa761
Jun <junoh421@gmail.com>
2018-01-01 15:03:21 -0500

    able to send message on the front end

--- src/actions/index.js
+++ src/actions/index.js
@@ -61,11 +61,11 @@ export const goToDashboard = () => {
 }
 
 export const sendMessage = ({ content, userId }) => {
-  // let conversationId = '5a4a7c9c91dd88c86c9925a0';
-  debugger;
+  let conversationId = '5a4a7c9c91dd88c86c9925a0';
   return function(dispatch) {
-    axios.post(`${ROOT_URL}/api/message`, { content, userId })
+    axios.post(`${ROOT_URL}/message`, { content, userId, conversationId })
     .then( response => {
+      // dispatch({ type: 'FETCH_MESSAGES', payload: response.data });
     })
   }
 }
