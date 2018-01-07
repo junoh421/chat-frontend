e00f5ae95f08d49e554b0ee0ce94d1690f34492c
Jun <junoh421@gmail.com>
2018-01-07 04:08:39 -0500

    testing with socket io and connecting to backend

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,69 @@
 import React, { Component } from 'react';
 import '../style/app.css';
 import Header from '../containers/header'
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:8000');
 
-export default class App extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: 'message ... ',
+      term: ''
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.messageReceive = this.messageReceive.bind(this);
+    this.messageSend = this.messageSend.bind(this);
+  }
+
+  componentDidMount() {
+    socket.on('send:message', this.messageReceive);
+  }
+
+  messageReceive(message) {
+    this.setState( {message : message} );
+  }
+
+  messageSend(message) {
+    socket.emit('send:message', message);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value})
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    socket.emit('send:message', this.state.term);
+    this.setState( {message: this.state.term} )
+    this.setState( {term: ''} )
+  }
 
   render() {
     return (
        <div className="App">
           <Header />
+          <p className="App-intro">
+          message: {this.state.message}
+          </p>
+          <form onSubmit={this.onFormSubmit}>
+            <div className="input-group">
+              <input
+               placeholder="Message..."
+               className="input-group"
+               value={this.state.term}
+               onChange={this.onInputChange}
+              />
+              <span className="input-group-btn">
+                <button type="submit" className="btn btn-primary">Send</button>
+              </span>
+            </div>
+          </form>
       </div>
     );
   }
 }
+
+export default App
